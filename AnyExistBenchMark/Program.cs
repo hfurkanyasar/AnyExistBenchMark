10f45a2ca8a355d5456eac5b9e96da11c548394b
hfurkanyasar <hfurkanyasar@gmail.com>
2023-09-26 19:29:39 +0300

    Add project files.

---
+++ AnyExistBenchMark/Program.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static AnyExistBenchMark.TextClass;
+namespace AnyExistBenchMark
+{
+    public class Program
+    {
+        public List<string> list = new List<string>();
+        public string searchItem = string.Empty;
+
+        public Program()
+        {
+            FillGuidList(ref list);
+        }
+
+        [Benchmark]
+        public bool TextAny()
+        {
+            return list.Any(a => a == searchItem);
+        }
+
+        [Benchmark]
+        public bool TextExist()
+        {
+            return list.Exists(a => a == searchItem);
+        }
+
+        public static void Main(string[] args)
+        {
+            
+            var summary = BenchmarkRunner.Run<Program>();
+            Console.ReadKey();
+        }
+
+        public void FillGuidList(ref List<string> list)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var item = Guid.NewGuid().ToString();
+                list.Add(item);
+                if (i == 5000) { Console.WriteLine(item); searchItem = item; }
+            }
+        }
+    }
+}
---
+++ AnyExistBenchMark/TextClass.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyExistBenchMark
+{
+    public class TextClass
+    {
+        public List<string> list = new List<string>();
+        public string searchItem = string.Empty;
+
+        public TextClass()
+        {
+            FillGuidList(ref list);        
+        }
+        [Benchmark]
+        public bool TextAny()
+        {
+            return list.Any(a => a == searchItem);
+
+        }
+        [Benchmark]
+        public bool TextExist()
+        {
+            return list.Exists(a => a == searchItem);
+        
+        }
+
+
+
+        public void FillGuidList(ref List<string> list)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var item = Guid.NewGuid().ToString();
+                list.Add(item);
+                if (i == 5000) { Console.WriteLine(item); searchItem = item; }
+            }
+        }
+    }
+}
